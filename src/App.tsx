fcf1559bf80750137fda176300c2872d9b6e3cd2
Arman <armangrigoryan1@mail.ru>
2023-10-09 16:10:53 +0200

    therti commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container App">
       <h1>App</h1>
     </div>
   );
