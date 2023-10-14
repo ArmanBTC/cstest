87fd888825fe21ce65f50ab461cd59f53ffabcec
Arman <armangrigoryan1@mail.ru>
2023-10-15 00:52:49 +0200

    getrequesttest commit

--- src/Layout/pages/Index.tsx
+++ src/Layout/pages/Index.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Index = () => {
+  const [test, setTest] = useState("null");
+
+  async function GetTEST() {
+    const response = await axios.get(
+      "https://armantest.azurewebsites.net/api/amd/getname"
+    );
+    const data = await response.data;
+    setTest(data);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    GetTEST();
+  }, []);
+
   return (
     <div className="indexPage">
       <div className="appColum-1"></div>
 
       <div className="container text-center appMargin">
         <div className="row align-items-center">
-          <div className="col">OUR GALERIA</div>
+          <div className="col">OUR GALERIA {test}</div>
         </div>
       </div>
 
